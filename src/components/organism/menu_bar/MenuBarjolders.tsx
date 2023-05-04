da9063db27fc0e78485a3c2b89250be4dad97ee7
debnix <daquipe5@gmail.com>
2023-05-04 17:27:42 -0500

    refactor: style maints and footer with margin-bottom

--- src/components/organism/menu_bar/MenuBarjolders.tsx
+++ src/components/organism/menu_bar/MenuBarjolders.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image'
 import './menu_barjolders.scss'
 import Jolder from '@/assets/jolder.png'
-import HamburgerIcon from '../../../assets/HamburgerIcon.png';
-
-
+import HamburgerIcon from '../../../assets/HamburgerIcon.png'
 
 const MenuBarJolders = () => {
 	const itemOptions = [
@@ -30,20 +28,22 @@ const MenuBarJolders = () => {
 	]
 
 	return (
-		<section className='menuBarjolders'>
-			<section className='logoMenuBarjolders'>
-				<Image src={ Jolder } alt='Logo jolders'/>
+		<section className="menuBarjolders">
+			<section className="logoMenuBarjolders">
+				<Image src={Jolder} alt="Logo jolders" />
 			</section>
-			<section className='optionsMenuBarjolders'>
-				{ itemOptions.map((item) => <a href={ item.redirecTo }>{ item.text }</a>) }
+			<section className="optionsMenuBarjolders">
+				{itemOptions.map((item) => (
+					<a key={item.text} href={item.redirecTo}>
+						{item.text}
+					</a>
+				))}
 			</section>
-			
-			<section className='hamburgericon'>
-			<Image  src={ HamburgerIcon } alt='Icon Hamburguer'/>
+
+			<section className="hamburgericon">
+				<Image src={HamburgerIcon} alt="Icon Hamburguer" />
 			</section>
-			
 		</section>
-		
 	)
 }
 
